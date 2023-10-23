214cd93868ba85e5f0e8894612b7218826d29ecb
James Ding <jamesding365@gmail.com>
2023-10-23 13:12:42 -0700

    Initial commit

---
+++ src/main/java/frc/robot/commands/PlaceholderCommand.kt
@@ -0,0 +1,11 @@
+package frc.robot.commands
+
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+class PlaceholderCommand : CommandBase()
+{
+    init
+    {
+        // Call addRequirements() here to declare subsystem dependencies.
+    }
+}
